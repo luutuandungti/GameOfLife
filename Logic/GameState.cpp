c1bd97bc2a4861bd0483be2f232e82fb75f77f79
Ti <luutuandungti@yahoo.com>
2019-07-21 03:06:22 +0300

    Temp logic

--- Logic/GameState.cpp
+++ Logic/GameState.cpp
@@ -3,10 +3,13 @@
 #define ALIVE true
 #define DEAD false
 
-//Return number of alive neighbors
+// Find number of alive neighbors around given cell
+// INPUT
 // int i, j                             : position of cell in array
 // int N                                : dimension of the board (NxN)
-// std::vector<std::vector<bool>>state  : board status represent by an array 
+// std::vector<std::vector<bool>>state  : board status represent by an array
+// 
+// RETURN number of neighbors 
 int number_of_neighbors(int i, int j, int N, std::vector<std::vector<bool>> state){    
     int Neighbors = 0;
     if (i>0) {
@@ -53,16 +56,19 @@ int number_of_neighbors(int i, int j, int N, std::vector<std::vector<bool>> stat
 }
 
 //Game Logic: return the next state of the cell
+// INPUT
 // int neighbors        :   number of alive neighbors of the cell
 // bool cell_state      :   current state of the cell
 // 
-// Rule: If the cell is alive:
+// RETURN state of the cell for next stage.
+//
+// Rules: If the cell is alive:
 //          1. If it has 1 or no neighbors, it will turn "dead". As if by solitude.
 //          2. if it has 4 or more neighbors, it will turn "dead". As if by overpopulation.
 //          3. If it has 2 or 3 neighbors, it will remain "alive".
 //        If the cell is dead:
 //          1. If it has exactly three neighbors, it will turn "alive", as if by regrowth.
-// Note: Based on the given rule, any number of neighbors that is not 2 or 3 will give the next cell state as DEAD.
+// Note: Based on the given rules, any number of neighbors that is not 2 or 3 will give the next cell state as DEAD.
 //       2 neighbors return same state
 //       3 neighbors return ALIVE
 bool game_logic(int neighbors, bool cell_state) {
@@ -79,7 +85,7 @@ bool game_logic(int neighbors, bool cell_state) {
 class GameState {
     private:
         int N;
-        std::vector<std::vector<bool>> state;
+        std::vector<std::vector<bool>> State;
         
     public:
         GameState (int n) {
@@ -90,14 +96,14 @@ class GameState {
                 for (int j = 0; i < N; j++) {
                     temp_bool_vector.push_back(DEAD);
                 }
-                state.push_back(temp_bool_vector);
+                State.push_back(temp_bool_vector);
             }
         }
         //Run this method to process to the next game state
         void nextstage() {            
             for (int i = 0; i < N ; i++){ 
                 for (int j = 0; i < N; j++) {
-                    state[i][j] = game_logic(number_of_neighbors(i,j,N,state), state[i][j]);           
+                    State[i][j] = game_logic(number_of_neighbors(i,j,N,State), State[i][j]);           
                 }
             }
         }
